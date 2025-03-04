4ee4f957e27eb766696405d73da6700aa3ccee9a
p-buddy <parkermalachowsky@gmail.com>
2025-03-04 19:13:42 +0000

    adding pane route

---
+++ src/routes/tests/pane/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { type PanelProps, PaneView } from "$lib/index.js";
+</script>
+
+{#snippet a({ params }: PanelProps<"pane", {}>)}
+  <div>a</div>
+{/snippet}
+
+{#snippet b({ params }: PanelProps<"pane", {}>)}
+  <div>b</div>
+{/snippet}
+
+<div style:width="100vw" style:height="100vh">
+  <PaneView
+    snippets={{ a, b }}
+    onReady={async ({ api }) => {
+      const { panel } = await api.addSnippetPanel("a", {});
+      api.addSnippetPanel("b", {});
+      //api.removePanel(panel);
+    }}
+  />
+</div>
